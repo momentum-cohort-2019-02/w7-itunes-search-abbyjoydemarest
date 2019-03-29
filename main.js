59e7a1c15ea73308f3a2ff2c8770a40983d3c511
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-29 12:31:52 -0400

    data fetching is happening

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 /* globals fetch */
+console.log('work?')
 
-
-document.addEventListener('DOMContentLoad', function () {
-    fetch('https://itunes-api-proxy.glitch.me/search?term=jack/johnson')
+function getArtistMusic() {
+    console.log('work?2')
+    return fetch(`https://itunes-api-proxy.glitch.me/search?term=jack+johnson`)
         .then(function (response) {
             if (!response.ok) {
-                throw Error(reponse.statusText)
+                console.log('work?3')
+                throw Error(response.statusText)
             }
+            console.log('work?4')
             return response.json()
-        }).then(function (json) {
-            console.log(json)
-        }).catch(function (error) {
-            console.log('Looks like there was a problem...boo', error)
         })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('work?5')
+    getArtistMusic()
+        .then(function (json) {
+            console.log('work?6')
+            console.log(json)
+        })
+        console.log('work?7')
 })
+console.log('work?8')
