ebb5d9c35e1a4b9846589bcea432fe16e5f57aa5
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-30 16:03:33 -0400

    changed querySelector to query with a fucntion

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 /* globals fetch */
 console.log('work?');
+function query(selector) {
+    return document.querySelector(selector)
+}
+function queryAll(selector) {
+    return document.querySelectorAll(selector)
+}
 
-function getArtistMusic() {
+function getArtistMusic(artistInput) {
     console.log('work?2')
-    return fetch(`https://itunes-api-proxy.glitch.me/search?term=jack+johnson`)
+    return fetch(`https://itunes-api-proxy.glitch.me/search?term=${artistInput}`)
         .then(function (response) {
             if (!response.ok) {
                 console.log('work?3')
@@ -13,26 +19,30 @@ function getArtistMusic() {
             return response.json()
         });
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('work?5')
-    getArtistMusic()
+function updateArtistMusic(artistiInput) {
+    getArtistMusic(artistInput)
         .then(function (artistMusicData) {
             console.log(artistMusicData)
             let idx
             for (idx = 0; idx < artistMusicData.results.length; idx++){
                 const artist = artistMusicData.results[idx].artistName
-                document.querySelector('#artist-name').innerHTML = artist
+                document.query('#artist-name').innerHTML = artist
                 const coverPhoto = artistMusicData.results[idx].artworkUrl30
-                document.querySelector('#image-url').innerHTML = coverPhoto
+                document.query('#image-url').innerHTML = coverPhoto
                 const trackName = artistMusicData.results[idx].trackName
-                document.querySelector('#track-name').innerHTML = trackName
+                document.query('#track-name').innerHTML = trackName
                     console.log(artist, 'yes')
                     console.log('work?6')
             }
 
-
             })
-        console.log('work?7')
+            }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('work?5')
+})
+document.query('#artist-input').addEventListener('change', function (event) {
+    updateAritistMusic(event.target.value)
 })
 console.log('work?8')
