4c21d42acdc436c2d81d400200a75feef7779304
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-31 14:23:49 -0400

    shows list of all songs

--- main.js
+++ main.js
@@ -1,50 +1,70 @@
 /* globals fetch */
-console.log('work?');
+
 function query(selector) {
-    return document.querySelector(selector)
+    return document.querySelector(selector);
 }
 function queryAll(selector) {
-    return document.querySelectorAll(selector)
+    return document.querySelectorAll(selector);
 }
 
 function getArtistMusic(artistInput) {
-    console.log('work?2')
+    console.log("work?2");
     return fetch(`https://itunes-api-proxy.glitch.me/search?term=${artistInput}`)
         .then(function (response) {
             if (!response.ok) {
-                console.log('work?3')
+                console.log("work?3");
                 throw Error(response.statusText);
             }
-            console.log('work?4');
-            return response.json()
+            console.log("work?4");
+            return response.json();
         });
 }
 function updateArtistMusic(artistInput) {
     getArtistMusic(artistInput)
         .then(function (artistMusicData) {
-            console.log(artistMusicData)
-            let idx
+            console.log(artistMusicData);
+            const listOfSongsDiv = document.getElementById('list-of-songs')
+            listOfSongsDiv.innerHtml = ""
+            let idx;
             for (idx = 0; idx < artistMusicData.results.length; idx++){
-                const artist = artistMusicData.results[idx].artistName
-                query('#artist-name').innerHTML = artist
-                const coverPhoto = artistMusicData.results[idx].artworkUrl30
-                query('#image-url').innerHTML = coverPhoto
-                const trackName = artistMusicData.results[idx].trackName
-                query('#track-name').innerHTML = trackName
-                    console.log(artist, 'work?5')
-                    console.log('work?6')
-            }
+                //AJ Bryce: create div with other divs in it to show all songs/info fir artist
+                const song = document.createElement('div')
+                const songDetails = document.createElement('div') 
+                const albumCover = document.createElement('div') 
+                console.log()
+                song.classList.add('song')
+                songDetails.classList.add('song-details')
+                albumCover.classList.add('cover-image')
 
+                //define all variables wanted from json
+                let artistName = artistMusicData.results[idx].artistName
+                let cover = artistMusicData.results[idx].artworkUrl30
+                let songName = artistMusicData.results[idx].trackName
+                console.log('artistName', artistName);
+                console.log('cover', cover);
+                console.log('songName', songName);
+                //enter those variables into the song div via the songDetails Div or albumCover div
+                songDetails.innerText = artistName + songName; albumCover.innerHtml = `<img src="${cover}"`
+                //query("#artist-name").innerHTML = artistName
+                //query("#artwork").innerHTML = `<img src="${cover}>`
+                //query("#track-name").innerHTML = songName
+                //query("#song").innerHTML = `${songTrack}`
+                console.log('songDetails', songDetails)
+                console.log('albumCover', albumCover)
+                song.appendChild(songDetails, albumCover)
+                listOfSongsDiv.append(song)
+                console.log('listOfSongsDiv', listOfSongsDiv)
+            }
             })
             }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('work?7')
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("work?7");
 
 
-query('#artist').addEventListener('change', function (event) {
-    updateArtistMusic(event.target.value)
-    console.log('work?8')
-}) 
-})
+    query("#artist").addEventListener("change", function (event) {
+        updateArtistMusic(event.target.value);
+        console.log("work?8");
+    })
+});
